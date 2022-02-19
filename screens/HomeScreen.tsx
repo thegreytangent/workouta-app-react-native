2ffb221c582ccd03905cbea0a9bae4a68151f373
thegreytangent <thegreytangent@gmail.com>
2022-02-19 09:54:42 +0800

    Adding data.json in workout app

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,17 +1,34 @@
 import { useEffect } from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text, Button, StyleSheet, FlatList} from 'react-native';
 import { NativeStackHeaderProps } from "@react-navigation/native-stack";
-
+import data from "../data.json";
 
 export default function HomeScreen({navigation} : NativeStackHeaderProps ) {
     useEffect( () => {
         console.log("Initializing HomeScreen ");
         return () => console.log("Unmounting Home Screen")
     },[])
+    const renderItem = ({item}: any) => {
+        return (
+            <View>
+            <Text>{item.name}</Text>
+            <Text>{item.difficulty}</Text>
+        </View>
+        );
+    }
     return (
-        <View>
-            <Text>I am home screen</Text>
-          <Button title='Goto Test' onPress={() => navigation.navigate("Test")} />
+        <View style={styles.container}>
+             <FlatList 
+              data={data} 
+              keyExtractor={item => item.slug} //for unique key
+             renderItem={renderItem}
+             />
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 20
+    }
+});
